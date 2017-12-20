06ab68f905820d8719e3c5a861491ae93e8fe3c2
Dan Zera <dczera@gmail.com>
2017-12-19 19:18:44 -0600

    move data to JS file, add select event handler

--- client.js
+++ client.js
@@ -1,6 +1,43 @@
 $(document).ready(function() {
 	console.log('doc loaded');
+	
+
+	var data = [{
+		id: 1,
+		text: "Ford"
+	}, {
+		id: 2,
+		text: "Chevrolet"
+	}, {
+		id: 3,
+		text: "Volvo"
+	}, {
+		id: 4,
+		text: "Saab"
+	}, {
+		id: 5,
+		text: "Mercedes"
+	}, {
+		id: 6,
+		text: "Audi"
+	}, {
+		id: 7,
+		text: "Subaru"
+	}];
+
 	$('.fancy-select').select2({
-		placeholder: 'Select an auto maker'
+		placeholder: 'Select an auto maker',
+		data: data,
+		width: '100%'
+	}).on({
+		'select2:select': function(e) {
+			// do something with selected item
+			// this displays an array with one object representing the item that was selected
+			console.log($('.fancy-select').select2('data'));
+			var selected_car = $('.fancy-select').select2('data')[0];
+			console.log(selected_car);
+			var car_id = $('.fancy-select').select2('data')[0].id;
+			console.log(car_id);
+		}
 	});
 });
